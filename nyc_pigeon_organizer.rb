28aa96c0ac3455439591c415bc5c43da8d6d7689
mystical-calculator-5374 <bbrower@smu.edu>
2020-01-25 05:37:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,18 +6,32 @@ def nyc_pigeon_organizer(data)
   # pp data
   result = {}
 
-  data.each do |first_key, sub_keys|
-    sub_keys.each do |attribute, array|
+  data.each do |first_key, hash|
+    has.each do |attribute, array|
       array.each do |name|
-        result[name] = {
-          color: [],
-          gender: [],
-          lives: []
-        }
+        if !result.has_key?(name)
+          result[name] = {}          
+        end
+        if !result[name].has_key?(first_key)
+          result[name][first_key] = []          
+        end
+        if !result[name][first_key].has_key?(attribute)
+          result[name][first_key] << "#{attribute}"          
+        end
       end
     end
   end
-
+  # data.each do |first_key, sub_keys|
+  #   sub_keys.each do |attributes, array|
+  #     array.each do |name|
+  #       result[name] = {
+  #         color: [],
+  #         gender: [],
+  #         lives: []
+  #       }
+  #     end
+  #   end
+  # end
 result
 
 
